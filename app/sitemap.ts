1fa5d528463c44f009521f5b9e440adaa899d0b4
Serter Iyigunlu <serteri@hotmail.com>
2025-06-19 22:41:00 +1000

    new file added

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 import { MetadataRoute } from 'next';
-import { getAllPosts } from './lib/posts'; // Post fonksiyonumuzu import ediyoruz
+import { getAllPosts } from '@/lib/posts';
 
 export default function sitemap(): MetadataRoute.Sitemap {
     const siteUrl = 'https://altiqdesign.com'; // KENDİ DOMAINİNİZİ YAZIN
