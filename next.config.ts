4c722f6f8297d456cbff63efd2cc64f6e2f37f84
Serter Iyigunlu <serteri@hotmail.com>
2026-02-18 00:36:51 +1000

    fix(config): Ignore NextConfig type error on build

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // @ts-ignore
   eslint: {
     // Uyarı: Bu ayar ESLint hatalarını üretim (production) yapısında yok sayar.
     ignoreDuringBuilds: true,
